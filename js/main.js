f9f31998c652522a53b9291ab1d33924d4ca4b25
Chester Cabahug <chesterdeveloper2000@gmail.com>
2021-10-14 19:20:34 +0800

    2>4 my attempt to do the challege! 😄

--- js/main.js
+++ js/main.js
@@ -22,20 +22,34 @@ function init() {
   //   markers: true,
   // }})
 
+  // // 2>3 simple parallax
+  // const parallaxTl = gsap.timeline({
+  //   ease: "none",
+  //   scrollTrigger: {
+  //     trigger: ".bcg-parallax",
+  //     start: "top bottom",
+  //     scrub: true,
+  //     markers: true
+  //   }
+  // })
+  // parallaxTl
+  //   .from(".content-wrapper", {duration: 0.4, autoAlpha: 0}, 0.4)
+  //   .from(".bcg", {duration: 2, y:"-30%"}, 0)
 
-  // 2>3 simple parallax
-  const parallaxTl = gsap.timeline({
+  // 2>4 simple pin example
+  gsap.to(["#intro h1", "#intro p"], {
+    duration: 1,
+    autoAlpha: 0,
     ease: "none",
     scrollTrigger: {
-      trigger: ".bcg-parallax",
-      start: "top bottom",
+      trigger: "#intro .content",
+      pin: true,
+      start: "top-=100 top",
+      end: "bottom top+=250",
       scrub: true,
-      markers: true
-    }
-  })
-  parallaxTl
-    .from(".content-wrapper", {duration: 0.4, autoAlpha: 0}, 0.4)
-    .from(".bcg", {duration: 2, y:"-30%"}, 0)
+      markers: true,
+    },
+  });
 }
 
 window.addEventListener("load", function () {
