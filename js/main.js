59d3a48ab5b125908ceca4ed051f83b2af5f7f44
Chester Cabahug <chesterdeveloper2000@gmail.com>
2021-10-14 17:40:43 +0800

    2>1 animating using scroll trigger a simple tween

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 gsap.registerPlugin(ScrollTrigger);
 
 function init() {
-  gsap.to("#intro img", {
-    opacity: 0,
-    scrollTrigger: {
-      trigger: "#intro",
-      start: "top top",
-      end: "bottom center",
-      scrub: true,
-      markers: true,
-    },
-  });
+  // 2>1 scrollTrigger basics
+  // gsap.to("#intro img", {
+  //   opacity: 0,
+  //   scrollTrigger: {
+  //     trigger: "#intro",
+  //     start: "top top",
+  //     end: "bottom center",
+  //     scrub: true,
+  //     markers: true,
+  //   },
+  // });
 }
 
 window.addEventListener("load", function () {
