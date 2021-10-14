fe1ea59827f17626b296e0dcc9fb8de901c5d946
Chester Cabahug <chesterdeveloper2000@gmail.com>
2021-10-14 17:29:18 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+gsap.registerPlugin(ScrollTrigger);
+
+function init() {
+  gsap.to("#intro img", {
+    opacity: 0,
+    scrollTrigger: {
+      trigger: "#intro",
+      start: "top top",
+      end: "bottom center",
+      scrub: true,
+      markers: true,
+    },
+  });
+}
+
+window.addEventListener("load", function () {
+  init();
+});
