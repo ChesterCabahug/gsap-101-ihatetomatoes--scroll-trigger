3c884dabf11fe8fc97d99e05f698ae3ff1c2ab3e
Chester Cabahug <chesterdeveloper2000@gmail.com>
2021-10-14 18:24:48 +0800

    2>2 learned about toggling classes with scrollTrigger! 😄

--- js/main.js
+++ js/main.js
@@ -12,6 +12,15 @@ function init() {
   //     markers: true,
   //   },
   // });
+
+  // 2>2 toggle css class
+  gsap.set("#project02", {scrollTrigger: {
+    trigger: "#project02",
+    start: "top bottom-=150",
+    end: "bottom center-=150",
+    toggleClass: "active",
+    markers: true,
+  }})
 }
 
 window.addEventListener("load", function () {
